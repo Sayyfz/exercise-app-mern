2d5c43841ce25df3d429c54674b34f501adcd7aa
Seif Ashraf <seif_fire2@hotmail.com>
2022-11-07 19:06:21 +0200

    all https requests created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import workoutRoutes from './routes/workouts.js';
+//loads the .env file variables into the process.env object
+import dotenv from 'dotenv'; 
 
 const app = express();
-const port = process.env.PORT || 5000
+
+dotenv.config(); // this is the method that attaches the .env variables to process.env
+const port = process.env.PORT || 5000;
+
 //midlewares
-// app.use();
+app.use(express.json()); // It looks if there is any json data sent in https requests and it attches it to req object
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
-})
+});
